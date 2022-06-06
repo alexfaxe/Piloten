f28527e81cae2fde5dcf6eadfd9059ea614d9817
alexfaxe <alexfaxe@gmail.com>
2022-06-06 16:08:37 +0200

    Change coinflip number

--- commands/flip.ts
+++ commands/flip.ts
@@ -4,7 +4,7 @@ export default {
     callback: (message: Message) => {
         const result = Math.random();
         console.log(result);
-        if (result > 0.49999999999999999) {
+        if (result > 0.5) {
             return message.reply("Heads");
         } else {
             return message.reply("Tails");
